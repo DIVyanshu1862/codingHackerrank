3d955a8b1406b41204ab80fd129bb11e36807bfd
DIVyanshu1862 <144006346+DIVyanshu1862@users.noreply.github.com>
2023-09-04 14:30:07 +0530

    Create First.java

---
+++ Java Coding day1/First.java
@@ -0,0 +1,10 @@
+Public class First
+  {
+    public static void main(String args[])
+    {
+      System.out.println("My first program in java");
+    }
+  }
+
+
+// Output : My first program in java
